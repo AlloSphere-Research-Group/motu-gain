3fa65804eac382f24131895f188201805a5225c7
Andres Cabrera <mantaraya36@gmail.com>
2022-03-04 10:39:07 -0800

    Adjustments to use curl

--- src/test.cpp
+++ src/test.cpp
@@ -3,12 +3,7 @@
 #include "al/app/al_App.hpp"
 #include "al/ui/al_ParameterGUI.hpp"
 
-//#define USE_CURL
-
-#ifdef USE_CURL
-#include "curl/curl.h"
-
-#endif
+#define USE_CURL
 
 struct AVBDevice {
   std::string host;
@@ -27,9 +22,9 @@ struct MyApp : App {
   Trigger volumeDown{"Down"};
 
   std::vector<AVBDevice> devices = {
-      {"169.254.203.42", 9998, 0, 16},
-      {"169.254.156.178", 9998, 0, 24},
-      {"169.254.124.144", 9998, 0, 24},
+      {"16A.local", 9998, 0, 16},
+      {"24Ao.local", 9998, 0, 24},
+      {"24Ao-2.local", 9998, 0, 24},
   };
   std::vector<std::string> simulatorMachines = {"localhost", "audio.1g",
                                                 "ar01.1g"};
@@ -98,11 +93,13 @@ struct MyApp : App {
 #ifdef USE_CURL
       for (auto device : devices) {
         for (size_t i = 0; i < device.numChannels; i++) {
-          std::string address = "http://" + device.host + "/datastore/ext/";
-          address += "obank/" + std::to_string(device.bankIndex) + "/";
-          address += "ch/" + std::to_string(i) + "/trim";
-          std::string data = "json={\"value\":-24}";
-          httpPost(address, data);
+          std::string address = device.host + "/datastore";
+          std::string jsonData = "{ \"ext/obank/" +
+                                 std::to_string(device.bankIndex) + "/ch/" +
+                                 std::to_string(i) + "/trim\" : -60}";
+          std::string command =
+              "curl --data \"json=" + jsonData + "\" " + address;
+          system(command.c_str());
         }
       }
 #else
@@ -128,12 +125,16 @@ struct MyApp : App {
     for (auto device : devices) {
       for (size_t i = 0; i < device.numChannels; i++) {
 
-        std::string address = "http://" + device.host + "/datastore/ext/";
-        address += "obank/" + std::to_string(device.bankIndex) + "/";
-        address += "ch/" + std::to_string(i) + "/trim";
-        std::string data =
-            "json={\"value\":" + std::to_string(int(volume)) + "}";
-        httpPost(address, data);
+        std::string address = device.host + "/datastore";
+        std::string jsonData = "{ \"ext/obank/" +
+                               std::to_string(device.bankIndex) + "/ch/" +
+                               std::to_string(i) +
+                               "/trim\" : " + std::to_string(int(volume)) + "}";
+        std::string command =
+            "curl --data \"json=" + jsonData + "\" " + address;
+        system(command.c_str());
+      }
+    }
 #else
 
     for (const auto &device : devices) {
@@ -150,32 +151,32 @@ struct MyApp : App {
     }
 
 #endif
-      }
+  }
 
-      void setLevel(float level) {
-        volume = level;
-        sendVolume();
-      }
+  void setLevel(float level) {
+    volume = level;
+    sendVolume();
+  }
 
 #ifdef USE_CURL
-      // HTTP requests
-      void httpPost(std::string address, std::string data) {
-        CURL *curl_handle;
-        CURLcode res;
+  // HTTP requests
+//    void httpPost(std::string address, std::string data) {
+//      CURL *curl_handle;
+//      CURLcode res;
 
-        curl_global_init(CURL_GLOBAL_ALL);
+//      curl_global_init(CURL_GLOBAL_ALL);
 
-        curl_handle = curl_easy_init();
-        curl_easy_setopt(curl_handle, CURLOPT_URL, address.c_str());
-        //      curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION,
-        //      WriteMemoryCallback); curl_easy_setopt(curl_handle,
-        //      CURLOPT_WRITEDATA, (void *)&chunk);
-        curl_easy_setopt(curl_handle, CURLOPT_POSTFIELDS, data.data());
-        curl_easy_setopt(curl_handle, CURLOPT_POSTFIELDSIZE, -1L);
-        curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+//      curl_handle = curl_easy_init();
+//      curl_easy_setopt(curl_handle, CURLOPT_URL, address.c_str());
+//      //      curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION,
+//      //      WriteMemoryCallback); curl_easy_setopt(curl_handle,
+//      //      CURLOPT_WRITEDATA, (void *)&chunk);
+//      curl_easy_setopt(curl_handle, CURLOPT_POSTFIELDS, data.data());
+//      curl_easy_setopt(curl_handle, CURLOPT_POSTFIELDSIZE, -1L);
+//      curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
 
-        res = curl_easy_perform(curl_handle);
-      }
+//      res = curl_easy_perform(curl_handle);
+//    }
 
 //  static size_t
 //  WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
@@ -199,12 +200,12 @@ struct MyApp : App {
 //    return realsize;
 //  }
 #endif
-    };
+};
 
-    int main() {
-      MyApp app;
-      app.configureAudio(48000, 512, 0,
-                         0); // 0 channels makes the domain not run
-      app.dimensions(600, 400);
-      app.start();
-    }
+int main() {
+  MyApp app;
+  app.configureAudio(48000, 512, 0,
+                     0); // 0 channels makes the domain not run
+  app.dimensions(600, 400);
+  app.start();
+}
